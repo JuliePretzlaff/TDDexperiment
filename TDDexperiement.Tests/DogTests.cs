3625fb8f3620c823e373075f82f3190659b64add
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-02-01 15:02:26 -0500

    initial commit

---
+++ TDDexperiement.Tests/DogTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDexperiment;
+
+namespace TDDexperiement.Tests
+{
+    [TestFixture]
+    public class DogTests
+    {
+        [Test]
+        public void DoesDogClassExist()
+            {
+            Dog dogOne = new Dog();
+            Assert.That(dogOne, Is.InstanceOf<Dog>());
+            }
+
+        [Test]
+        public void DoesDogHaveAge()
+        {
+            Dog dogOne = new Dog();
+
+            dogOne.ReturnDogAge(2);
+
+            Assert.That(dogOne.DogAge, Is.EqualTo(2));
+        }
+
+      
+    }
+}
---
+++ TDDexperiment/Dog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDexperiment
+{
+    public class Dog
+    {
+        //fields
+        private string dogName;
+        private int dogAge;
+
+        //properties
+        public string DogName
+        {
+            get { return this.dogName; }
+            set { this.dogName = value; }
+        }
+
+        public int DogAge
+        { 
+            get { return this.dogAge; }
+            set { this.dogAge = value; }
+        }
+
+        //constructors
+        public Dog()
+        {
+            //default constructor
+        }
+
+        public Dog(string name, int age)
+        {
+            this.dogName = name;
+            this.dogAge = age;
+        }
+
+        //methods
+        public void ReturnDogAge(int age)
+        {
+            DogAge = age;
+        }
+
+
+    }
+}
